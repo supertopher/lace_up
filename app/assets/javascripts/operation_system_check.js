8b1cb602a9b8e7cd8771549b233a9bdd539f8e21
supertopher <asimplechris@gmail.com>
2013-08-15 14:58:37 -0700

    Added OS check JS and basic app layout view
    Basic JS call to return a value for the current detected OS.  This will be used later for specific instructions by operating system.
    Rewrote application layout in HAML.
    Very Basic style added for layout.

---
+++ app/assets/javascripts/operation_system_check.js
@@ -0,0 +1,9 @@
+$(function() {
+    console.log( OSName );
+});
+
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
