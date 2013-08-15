6ab559a50a203dbffc198f262e330316d7ca2054
supertopher <asimplechris@gmail.com>
2013-08-15 12:15:55 -0700

    Initial Commit
    Removed default pg usernames, installed gems for rspec, capybara, better errors
    No test unit included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaceUp::Application.config.secret_token = '4c05358ea981624bab49b783166db07f0b00d1f5a5c254fe5cf009a912087e1df4feaf6b84cebdd0776fec076dc6e0a6469e6befca07bdb8865c1c37f12b02f8'
